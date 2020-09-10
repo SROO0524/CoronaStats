8117eb16622f2d3135d1b4a58aa626b0b00619e8
SROO0524 <vmflstm847@naver.com>
2020-09-10 22:35:02 +0900

    Initial Commit

---
+++ CoronaStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaStats
+//
+//  Created by 김믿음 on 2020/09/10.
+//  Copyright © 2020 김믿음. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
