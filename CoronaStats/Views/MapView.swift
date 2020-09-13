ff7a52cf3c190d8b964b75fa877978d2a8d34f93
SROO0524 <vmflstm847@naver.com>
2020-09-14 01:49:20 +0900

    Ect

---
+++ CoronaStats/Views/MapView.swift
@@ -0,0 +1,16 @@
+//
+//  MapView.swift
+//  CoronaStats
+//
+//  Created by 김믿음 on 2020/09/12.
+//  Copyright © 2020 김믿음. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    
+}
+
