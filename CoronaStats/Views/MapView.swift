4d0b44e3158aa9ab5e3b2dc8007b9efdeedb1cc0
SROO0524 <vmflstm847@naver.com>
2020-10-23 15:10:33 +0900

    Creating map View

--- CoronaStats/Views/MapView.swift
+++ CoronaStats/Views/MapView.swift
@@ -6,11 +6,43 @@
 //  Copyright © 2020 김믿음. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
 import UIKit
 import MapKit
 
 struct MapView : UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        return MKMapView()
+    }
+
+    @Binding var countryData : [CountryData]
     
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        // 코로나가 발생한 모든 지역에 핀 표기 하는것. 처음에는 빈 값으로 두고, countryData에서 값을 받아 넣는다.
+        var allAnnotations : [CoronaCaseAnnotation] = []
+        
+        // 핀 아래 텍스트에 확진자와 사망자 표기!
+        for data in countryData {
+            // \n >> 다음줄에 표기
+            let title = data.country + "\n Confirmed " + data.confirmed.formatNumber() + "\n Death " + data.deaths.formatNumber()
+            
+            let coordinate = CLLocationCoordinate2D(latitude: data.latitude, longitude: data.longitude)
+            
+            allAnnotations.append(CoronaCaseAnnotation(title: title, coordinate: coordinate))
+        }
+        
+        uiView.annotations.forEach{uiView.removeAnnotation($0)}
+        uiView.addAnnotation(allAnnotations as! MKAnnotation)
+    }
 }
 
+class CoronaCaseAnnotation : NSObject,MKAnnotation {
+    
+    let title: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.coordinate = coordinate
+    }
+}
